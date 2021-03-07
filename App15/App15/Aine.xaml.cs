774714a5a659289fd83f7959db292f18ee75083e
tluur <tluur@tlu.ee>
2021-03-07 20:16:55 +0200

    Add project files.

---
+++ App15/App15/Aine.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App15
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Aine : ContentPage
+    {
+        public Aine()
+        {
+            InitializeComponent();
+        }
+    }
+}
