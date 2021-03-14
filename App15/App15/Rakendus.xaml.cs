2b367e247f370f253a6c6ce5d2a0696d4ea1b70c
tluur <tluur@tlu.ee>
2021-03-14 14:31:06 +0200

    Aku laadimise levelil muudab värvi

--- App15/App15/Rakendus.xaml.cs
+++ App15/App15/Rakendus.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace App15
 {
@@ -15,6 +16,48 @@ namespace App15
         public Rakendus()
         {
             InitializeComponent();
+            SetBackground(Battery.ChargeLevel,
+            Battery.State == BatteryState.Charging);
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Battery.BatteryInfoChanged -= Battery_BatteryInfoChanged;
+        }
+
+        void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            SetBackground(e.ChargeLevel, e.State == BatteryState.Charging);
+        }
+
+        void SetBackground(double level, bool charging)
+        {
+            Color? color = null;
+            var status = charging ? "Charging" : "Not charging";
+
+            if(level > .5f)
+            {
+                color = Color.Green.MultiplyAlpha(level);
+            }
+            else if(level > .1f)
+            {
+                color = Color.Yellow.MultiplyAlpha(1d - level);
+            }
+            else
+            {
+                color = Color.Red.MultiplyAlpha(1d - level);
+            }
+            BackgroundColor = color.Value;
+            LabelBatteryLevel.Text = status;
         }
     }
 }
